41a5338b571b5b011602421fee83e9a92ca41182
Mike Johnston <mrbean355@gmail.com>
2021-10-10 14:43:42 +0200

    Initial commit

---
+++ demo-app/src/main/kotlin/com/github/mrbean355/android/demo_app/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.github.mrbean355.android.demo_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.mrbean355.android.demo_app.ui.theme.MyApplicationTheme
+import com.github.mrbean355.compose.snackbar.Snackbar
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                    Snackbar()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
---
+++ lib/src/main/kotlin/com/github/mrbean355/compose/snackbar/Snackbar.kt
@@ -0,0 +1,8 @@
+package com.github.mrbean355.compose.snackbar
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Snackbar() {
+    // TODO
+}
